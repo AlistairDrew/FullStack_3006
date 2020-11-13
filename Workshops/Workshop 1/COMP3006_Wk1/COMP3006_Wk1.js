3b89ae31fe2d1d61ade659c0a3b0f0363d5b74a9
AlistairDrew <34186036+AlistairDrew@users.noreply.github.com>
2020-11-13 16:27:07 +0000

    Adding Workshop Material

---
+++ Workshops/Workshop 1/COMP3006_Wk1/COMP3006_Wk1.js
@@ -0,0 +1,26 @@
+function changeColour() {
+    var el=document.getElementsByClassName('light')[0];
+   
+    
+   
+       if (rgb2hex(el.style.backgroundColor) == "#00ffff") 
+       {
+         el.style.backgroundColor = "#ffff00";
+       }
+   
+    
+   
+   
+       else if (rgb2hex(el.style.backgroundColor) == "#ffff00") {
+         el.style.backgroundColor = "#ff00ff ";
+       } 
+       else if (rgb2hex(el.style.backgroundColor) == "#ff00ff") {
+        el.style.backgroundColor = "#00ffff";
+      }
+      else {
+   
+    
+   
+        el.style.backgroundColor = "#00ffff";
+      }
+   }
