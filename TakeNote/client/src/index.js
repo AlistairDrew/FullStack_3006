ea7e18e54821c0455ff0c2d2edc9c725619d47e4
AlistairDrew <34186036+AlistairDrew@users.noreply.github.com>
2021-01-27 13:12:24 +0000

    Updating index.js

--- TakeNote/client/src/index.js
+++ TakeNote/client/src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import './index.css';
+//import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App(){
+
+  const [message,setMessage] = useState("Hello world");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setMessage("Hi There");
+    }, 2000);
+  }, []); //empty dependency array makes component run once, prevents infinite loop and memory leak
+
+  
+
+  return(
+    <>
+  <Welcome user = "Jane Doe" message={message}/>
+  <Welcome user = "John Doe" message={message}/>
+  <Welcome user = "Peter Doe" message={message}/>
+  </>
+  );
+}
+
+function Welcome(props){
+  return (
+  <>
+  <h1>Welcome {props.user} to this site</h1>
+  <p>{props.message}</p>
+  </>
+  );
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+
+
+
+
+
+
