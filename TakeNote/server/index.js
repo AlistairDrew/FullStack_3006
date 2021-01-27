251e4eab33115c6bea78f976bf85fdf1cb3b4f96
AlistairDrew <34186036+AlistairDrew@users.noreply.github.com>
2021-01-27 05:34:12 +0000

    Created MongoDB

--- TakeNote/server/index.js
+++ TakeNote/server/index.js
@@ -12,3 +12,16 @@ app.listen(5000, () => console.log("server started on port 5000")); //deciding t
 //set up routers
 
 app.use("/snippet", require("./routers/snippetRouter")); //whenever request comes in with /snippet use this (snippetRouter) as the middleware
+
+//Connect to mongoDB
+
+mongoose.connect("mongodb+srv://takenote:csvOV4pOSidwSeJN@take-note.j23uw.mongodb.net/main?retryWrites=true&w=majority", {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, (err) =>{
+    if (err) return console.error(err);
+    console.log("Connected to MongoDB");
+});
+
+//csvOV4pOSidwSeJN
+//mongodb+srv://takenote:<password>@take-note.j23uw.mongodb.net/<dbname>?retryWrites=true&w=majority
--- TakeNote/server/routers/snippetRouter.js
+++ TakeNote/server/routers/snippetRouter.js
@@ -2,13 +2,13 @@ const router = require("express").Router();
 const Snippet = require("../models/snippetModel");
 
 router.post("/", (req,res) =>{
-    const {title, description, code} = req.body;
+    const {title, description, code} = req.body; //Send title desc and code
     
     const newSnippet = new Snippet({
-        title, description, code
+        title, description, code      //create new snippet using model created from mongoose
     });
 
-    newSnippet.save();
+    newSnippet.save(); //save snippet to database
 });
 
 module.exports = router;
