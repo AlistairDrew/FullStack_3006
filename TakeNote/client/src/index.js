0f0867ee17cb8be4310865d403d5c5673619b00c
AlistairDrew <34186036+AlistairDrew@users.noreply.github.com>
2021-01-27 20:23:58 +0000

    All applications of useEffect

--- TakeNote/client/src/index.js
+++ TakeNote/client/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; //add in useEffect
+import React, { useState, useEffect } from 'react'; //add in useEffect
 import ReactDOM from 'react-dom';
 //import './index.css';
 //import App from './App';
@@ -6,36 +6,51 @@ import ReactDOM from 'react-dom';
 
 function App() {
 
-const [formUsename, setFormUsername] = useState("");
-const [formPassword, setFormPassword] = useState("");
 
-    function sendData(e){
-      e.preventDefault();
-      
-      //sending data
-      setFormUsername("");
-      setFormPassword("");
+const [messageShown, setMessageShown] = useState(false);
 
-    }
+//First way of using useEffect
+useEffect(() =>{ //component update side effect, will be triggered any value is changed.
+  console.log("Component updating");
+});
+
+
+
+//Third way of using useEffect,puts specificity onto when to call useEffect , this one only calls whenever messageShown is changed 
+useEffect(() =>{ 
+  console.log("Specific state update");
+},[messageShown]);
+
+function Random(){
+  const[randomNumber, setRandomNumber] = useState(Math.random()); 
+  //Second way of using useEffect also the most common way of using useEffect, with empty dependency array.
+useEffect(() =>{
+  console.log("Component mount")
+  const intervalId = setInterval(() => {
+    setRandomNumber(Math.random());
+  }, 1000);
+  
+return () => { //Fourth way to apply useEffect, when unmounting certain things
+  console.log("Compnent Unmount");
+  clearInterval(intervalId);
+}
+
+}, []);
+return <h1>{randomNumber}</h1>
+}
   
 
   return (
     <>
-     <form onSubmit={sendData}>
-       <input type="text" 
-       placeholder="Username" 
-       onChange={(e)=> setFormUsername(e.target.value)}
-       value={setFormUsername}
-       />
-
-       <input type="password" 
-       placeholder="Password"
-       onChange={(e)=> setFormPassword(e.target.value)}
-       value={setFormPassword}
-       />
-       <button type="submit" placeholder="Submit">Log in</button> 
-     </form>
-
+      
+      <button onClick={() => setMessageShown(!messageShown)}>Toggle Message</button>
+      {messageShown && (
+      <>
+        <Random />
+        <p>Some Message</p>
+    
+      </>
+      )}
 
     </>
   );
