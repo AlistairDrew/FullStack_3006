3302285af1c5bd325f5fc8e25ec02747495f1660
AlistairDrew <34186036+AlistairDrew@users.noreply.github.com>
2021-01-27 05:04:53 +0000

    Created Snippet database model

--- TakeNote/Routers/snippetRouter.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-Router.get("/test", (req, res) => { //configure router with end point, request and response object 
-res.send("Router test");
-})
-
-module.exports = router;
