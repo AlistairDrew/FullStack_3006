edccc7d3064a13ca790d767842e70b0711eb1fc5
AlistairDrew <34186036+AlistairDrew@users.noreply.github.com>
2021-01-27 02:10:38 +0000

    Express server setup

--- TakeNote/server/index.js
+++ TakeNote/server/index.js
@@ -3,4 +3,10 @@ const mongoose = require("mongoose");
 
 // setup express server
 
+const app = express(); // function that constricts app object
 
+app.listen(5000, () => console.log("server started on port 5000")); //deciding the port what to run the express server on with error function
+
+app.get("/test", (req, res) => {
+    res.send("some more data");
+});
