30f94dfce2a8fdd008190b88aac9d0ec07ce62ac
AlistairDrew <34186036+AlistairDrew@users.noreply.github.com>
2021-01-27 05:55:53 +0000

    Database validation

--- TakeNote/server/routers/snippetRouter.js
+++ TakeNote/server/routers/snippetRouter.js
@@ -1,14 +1,26 @@
 const router = require("express").Router();
 const Snippet = require("../models/snippetModel");
 
-router.post("/", (req,res) =>{
-    const {title, description, code} = req.body; //Send title desc and code
-    
-    const newSnippet = new Snippet({
-        title, description, code      //create new snippet using model created from mongoose
-    });
+router.post("/", async (req,res) =>{ //also 
+    try {
+        const { title, description, code } = req.body; //Send title desc and code
 
-    newSnippet.save(); //save snippet to database
+        //Validation
+        if (!description && !code) {
+            return res.status(400).json({ errorMessage: "You need to enter at least a discription or some code" });
+        }
+
+        const newSnippet = new Snippet({
+            title, description, code      //create new snippet using model created from mongoose
+        });
+
+        const savedSnippet = await newSnippet.save(); //save snippet to database but also waits for promise to be resolved and returns document 
+
+        res.json(savedSnippet);
+    }
+    catch (err) {
+        res.status(500).send();
+    }
 });
 
 module.exports = router;
